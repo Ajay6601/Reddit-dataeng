576ab53a975ef94165520a13131f7452cc143522
Ajay6601 <ajayreddy6601@gmail.com>
2024-12-01 15:36:49 -0500

    etl

--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -4,9 +4,9 @@ from etls.reddit_etl import connect_reddit, extract_posts, transform_data, load_
 from utils.constants import CLIENT_ID, SECRET, OUTPUT_PATH
 
 
-def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=None):
+def reddit_pipeline(file_name: str, subreddit: str, time_filter='month', limit=None):
     # connecting to reddit instance
-    instance = connect_reddit(CLIENT_ID, SECRET, 'AJ')
+    instance = connect_reddit(CLIENT_ID, SECRET, 'de_app')
     # extraction
     posts = extract_posts(instance, subreddit, time_filter, limit)
     post_df = pd.DataFrame(posts)
